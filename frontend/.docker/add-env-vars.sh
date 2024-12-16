3b295965f0a7011ae85c3eeb28598540e971908f
MARCELO <marcelobraga.dev@gmail.com>
2024-12-16 07:47:26 -0300

    update

--- frontend/.docker/add-env-vars.sh
+++ frontend/.docker/add-env-vars.sh
@@ -5,29 +5,36 @@ _writeFrontendEnvVars() {
 }
 
 _writeNginxEnvVars() {
-    dockerize -template /etc/nginx/conf.d/default.conf:/etc/nginx/conf.d/default.conf
+    if dockerize -template /etc/nginx/conf.d/default.conf:/etc/nginx/conf.d/default.conf; then
+        echo "Nginx environment variables written successfully."
+    else
+        echo "Failed to write Nginx environment variables. Check the template configuration."
+    fi
 }
 
 _addSslConfig() {
-    SSL_CERTIFICATE=/etc/nginx/ssl/${1}/fullchain.pem;
-    SSL_CERTIFICATE_KEY=/etc/nginx/ssl/${1}/privkey.pem;
-    FILE_CONF=/etc/nginx/sites.d/${1}.conf
-    FILE_SSL_CONF=/etc/nginx/conf.d/00-ssl-redirect.conf;
+    local ENVIRONMENT="$1"
+    local SSL_CERTIFICATE="/etc/nginx/ssl/${ENVIRONMENT}/fullchain.pem"
+    local SSL_CERTIFICATE_KEY="/etc/nginx/ssl/${ENVIRONMENT}/privkey.pem"
+    local FILE_CONF="/etc/nginx/sites.d/${ENVIRONMENT}.conf"
+    local FILE_SSL_CONF="/etc/nginx/conf.d/00-ssl-redirect.conf"
 
-    if [ -f ${SSL_CERTIFICATE} ] && [ -f ${SSL_CERTIFICATE_KEY} ]; then
-        echo "saving ssl config in ${FILE_CONF}"
-        echo 'include include.d/ssl-redirect.conf;' >> ${FILE_SSL_CONF};
-        echo 'include "include.d/ssl.conf";' >> ${FILE_CONF};
-        echo "ssl_certificate ${SSL_CERTIFICATE};" >> ${FILE_CONF};
-        echo "ssl_certificate_key ${SSL_CERTIFICATE_KEY};" >> ${FILE_CONF};
+    if [ -f "${SSL_CERTIFICATE}" ] && [ -f "${SSL_CERTIFICATE_KEY}" ]; then
+        echo "SSL certificates found for ${ENVIRONMENT}, saving SSL config in ${FILE_CONF}"
+        echo 'include include.d/ssl-redirect.conf;' >> "${FILE_SSL_CONF}"
+        echo 'include "include.d/ssl.conf";' >> "${FILE_CONF}"
+        echo "ssl_certificate ${SSL_CERTIFICATE};" >> "${FILE_CONF}"
+        echo "ssl_certificate_key ${SSL_CERTIFICATE_KEY};" >> "${FILE_CONF}"
     else
-        echo 'listen 80;' >> ${FILE_CONF};
-        echo "ssl ${1} not found >> ${SSL_CERTIFICATE} -> ${SSL_CERTIFICATE_KEY}"
-    fi;
+        echo "SSL certificates not found for ${ENVIRONMENT}, using non-SSL configuration."
+        echo 'listen 80;' >> "${FILE_CONF}"
+        echo "ssl ${ENVIRONMENT} not found >> ${SSL_CERTIFICATE} -> ${SSL_CERTIFICATE_KEY}"
+    fi
 }
 
-_writeFrontendEnvVars;
-_writeNginxEnvVars;
+# Execute functions
+_writeFrontendEnvVars
+_writeNginxEnvVars
 
 _addSslConfig 'backend'
 _addSslConfig 'frontend'
